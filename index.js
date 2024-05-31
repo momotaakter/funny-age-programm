1373e6304983771c72b804fa1aab1036c03d09a0
momota akter <momotaa808@gmail.com>
2024-05-30 20:53:18 -0900

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+/*var age=prompt('enter your age:');
+var getres=age<=20?`tumi bier jonno upojokto noi
+tumi dekte akono arokom:<img src="a.jpg"><br><button>read more about your age</button>
+`:age>=20&&age<=30?`you are perfect for marrige`:`tomr otirikoto bois hoe gese ga`;
+document.write(getres);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var age=prompt('enter your age:');
+var getres=age<=20?`আপনি বিবাহের জন্য নিখুঁত নন আপনি এখনও শিশু :<img src="child.jpg"><br><button>read more about your age</button>
+`:age>=20&&age<=30?`আপনি বিবাহের জন্য উপযুক্ত...এটি আপনার জন্য উপযুক্ত সময়।<img src="young.jpg">`:`আপনার বয়স হয়েছে,,,তাই আপনার বিয়ের প্রোগ্রাম বাতিল করুন <img src="old.jpg">`;
+document.write(getres);
